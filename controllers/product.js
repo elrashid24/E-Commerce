c2a484dec9fee0a727e4df5f9ecccc6a9f7b32f8
elrashid24 <elrashide24@gmail.com>
2019-09-18 11:04:17 -0700

    adding product routes

--- controllers/product.js
+++ controllers/product.js
@@ -15,19 +15,54 @@ exports.create = (req, res) => {
                 error: 'Image could not be uploaded'
             })
         }
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All products must contain a name, description, price, category, quantity, and any relevant shipping information"
+            })
+        }
         let product = new Product(fields)
         if (files.photo) {
             //this should match the name of what's being sent from the client side (photo vs image vs picture etc)
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Pictures must be less than 1mb in size"
+                })
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
         product.save((err, result) => {
             if (err) {
                 return res.status(400).json({
-                    error: err
+                    error: errorHandler(err)
                 })
             }
             res.json(result)
         })
     })
+}
+
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Product not found"
+            })
+        }
+        req.product = product
+        next()
+    })
+}
+
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product)
 }
--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,9 @@ const {
     userById
 } = require('../controllers/user')
 const {
-    create
+    create,
+    productById,
+    read
 } = require('../controllers/product')
 
 const {
@@ -14,7 +16,7 @@ const {
 } = require("../controllers/auth")
 
 router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create)
-
+router.get('/product/:productId', read)
 router.param('userId', userById)
-
+router.param('productId', productById)
 module.exports = router
