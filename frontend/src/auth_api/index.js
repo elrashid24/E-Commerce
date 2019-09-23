f03a4a95c63caeb1668c250dabbb06f0bb0d5e5b
elrashid24 <elrashide24@gmail.com>
2019-09-23 14:41:38 -0700

    nav-item signout button

--- frontend/src/auth_api/index.js
+++ frontend/src/auth_api/index.js
@@ -34,3 +34,26 @@
           next()
       }
   }
+
+  export const signOut = (next) => {
+      if (typeof window !== 'undefined') {
+          localStorage.removeItem('jwt')
+          next()
+          //next = redirect user
+          return fetch(`http://localhost:8000/signout`, {
+              method: 'GET',
+          }).then(res => console.log('signout ', res)).catch(err => console.log('error ',
+              err))
+      }
+  }
+
+  export const isAuthenticated = (next) => {
+      if (typeof window === 'undefined') {
+          return false
+      }
+      if (localStorage.getItem('jwt')) {
+          return JSON.parse(localStorage.getItem('jwt'))
+      } else {
+          return false
+      }
+  }
--- frontend/src/core/Menu.jsx
+++ frontend/src/core/Menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Link, withRouter, Redirect } from "react-router-dom";
+import { signOut, isAuthenticated } from "../auth_api/index";
 
 const Menu = ({ history }) => {
   const activeLink = (history, path) => {
@@ -17,24 +18,48 @@ const Menu = ({ history }) => {
               Home
             </Link>
           </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              to="/signin"
-              style={activeLink(history, "/signin")}
-            >
-              Sign in
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              to="/signup"
-              style={activeLink(history, "/signup")}
-            >
-              Sign up
-            </Link>
-          </li>
+
+          {!isAuthenticated() && (
+            <React.Fragment>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/signin"
+                  style={activeLink(history, "/signin")}
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/signup"
+                  style={activeLink(history, "/signup")}
+                >
+                  Sign up
+                </Link>
+              </li>
+            </React.Fragment>
+          )}
+
+          {isAuthenticated() && (
+            <span>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/signout"
+                  style={{ cursor: "pointer", color: "white" }}
+                  onClick={() =>
+                    signOut(() => {
+                      history.push("/");
+                    })
+                  }
+                >
+                  Sign Out
+                </Link>
+              </li>
+            </span>
+          )}
         </ul>
       </div>
     </div>
