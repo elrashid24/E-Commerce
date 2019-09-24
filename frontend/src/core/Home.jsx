b5d8ebe92467be0390a7607d1d4b80f55de0335b
elrashid24 <elrashide24@gmail.com>
2019-09-24 15:27:22 -0700

    adding product item

--- frontend/src/core/Home.jsx
+++ frontend/src/core/Home.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./core_util";
+import ProductItem from "./product_item";
 const Home = () => {
-  return <Layout title="My Project" description="Home" />;
-};
+  const [productsBySold, setProductsBySold] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
 
+  const loadProductsBySold = () => {
+    getProducts(`sold`).then(productsSortedBySold => {
+      if (productsSortedBySold.error) {
+        setError(productsSortedBySold.error);
+      } else {
+        setProductsBySold(productsSortedBySold);
+      }
+    });
+  };
+  const loadProductsByArrival = () => {
+    getProducts(`createdAt`).then(productsSortedByArrival => {
+      if (productsSortedByArrival.error) {
+        setError(productsSortedByArrival.error);
+      } else {
+        setProductsByArrival(productsSortedByArrival);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySold();
+  }, []);
+  return (
+    <Layout title="My Project" description="Home">
+      <h2 className="mb-4">Popular Products</h2>
+
+      {productsBySold.map((product, idx) => {
+        return <ProductItem key={idx} product={product} />;
+      })}
+    </Layout>
+  );
+};
 export default Home;
---
+++ frontend/src/core/core_util.js
@@ -0,0 +1,7 @@
+export const getProducts = (sortBy) => {
+    return fetch(`http://localhost:8000/products?sortBy=${sortBy}&order=desc&limit=6`, {
+        method: 'GET'
+    }).then(categories => {
+        return categories.json()
+    }).catch(error => console.log(error))
+}
